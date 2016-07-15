ec4cef96974aa28bba5034fd4aa77fd8a8df887a
gauravagrawal <gauravagrawal@XE-T-GAURAVA.XEBIAINDIA.LOCAL>
2016-07-15 15:36:44 +0530

    Added component scan and security jars

--- src/main/java/com/xebia/hr/config/Application.java
+++ src/main/java/com/xebia/hr/config/Application.java
@@ -1,27 +1,27 @@
 package com.xebia.hr.config;
 
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
-
-import java.nio.charset.Charset;
-import java.util.Arrays;
 
 /**
  * Created by anirudh on 13/07/15.
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.xebia.hr"} )
 @EntityScan(basePackages = {"com.xebia.hr.entity"})
 @EnableJpaRepositories(basePackages = {"com.xebia.hr.repository"})
 public class Application extends SpringBootServletInitializer {
