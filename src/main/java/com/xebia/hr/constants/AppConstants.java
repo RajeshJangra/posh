d8a383a93e96b5e1118e912417a7fa58f5e45948
gauravagrawal <gauravagrawal@XE-T-GAURAVA.XEBIAINDIA.LOCAL>
2016-07-16 09:58:49 +0530

    Added constants and overriden the banner

---
+++ src/main/java/com/xebia/hr/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.xebia.hr.constants;
+
+public interface AppConstants {
+
+	/**
+	 * Courses's result states
+	 */
+	public String PASSED = "PASSED";
+	public String FAILED = "FAILED";
+	public String NOT_STARTED = "NOT STARTED";
+	
+}
