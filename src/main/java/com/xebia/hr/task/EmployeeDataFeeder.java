fc9dfbb04ae0a782102af88bc77bcbed3535874e
gauravagrawal <gauravagrawal@XE-T-GAURAVA.XEBIAINDIA.LOCAL>
2016-08-09 16:30:53 +0530

    Added logging

--- src/main/java/com/xebia/hr/task/EmployeeDataFeeder.java
+++ src/main/java/com/xebia/hr/task/EmployeeDataFeeder.java
@@ -7,32 +7,37 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.xebia.hr.entity.Course;
 import com.xebia.hr.entity.Employee;
+import com.xebia.hr.service.CourseService;
 import com.xebia.hr.service.EmployeeService;
 import com.xebia.hr.service.MailService;
 
 @Component
 public class EmployeeDataFeeder {
 
-	private Logger logger = Logger.getLogger(EmployeeDataFeeder.class);
+	private final static Logger logger = LoggerFactory.getLogger(EmployeeDataFeeder.class);
 
 	@Value("${mail.list}")
 	private String mailList;
@@ -48,6 +53,9 @@ public class EmployeeDataFeeder {
 
 	@Autowired
 	private MailService mailService;
+	
+	@Autowired
+	private CourseService courseService;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -59,10 +67,13 @@ public class EmployeeDataFeeder {
 	@Scheduled(cron="${induction.schedule.employee-feeder.cron}")
 	public void updateEmployees(){
 
+		logger.info("Calling EmployeeDataFeeder's updateEmployees() method");
 		List<Employee> employeeToBeUpdated = new ArrayList<>();
 		try {
 			List<Employee> employees = getEmployeeFromExcel();
 			List<Employee> existingEmployees = employeeService.findAllEmployees();
+			List<Course> courses = courseService.findAll();
+			Set<Course> uniqueCourses = new HashSet<>(courses);
 			for(Employee employee : employees){
 
 				Optional<Employee> existingEmployee = getExistingEmployee(existingEmployees, employee.getEmpId());
@@ -72,18 +83,22 @@ public class EmployeeDataFeeder {
 					existingEmployee.get().setDesignation(employee.getDesignation());
 					existingEmployee.get().setEmpType(employee.getEmpType());
 					employee = existingEmployee.get();
+					logger.info("Update existing employee: "+employee);
 				}else{
 					//By default, we set password as date of birth, format is ddMMyyyy like 02032013 - 2 March 2013
 					employee.setPassword( passwordEncoder.encode(passwordDateFormat.format(employee.getDateOfBirth())) );
 					employee.setLastPasswordReset(CURRENT_DATE);
 					employee.setActive(true);
+					logger.info("Add new employee: "+employee);
 				}
+				employee.setCourses(uniqueCourses);
 				employeeToBeUpdated.add(employee);
 			}
 			employeeService.save(employeeToBeUpdated);
+			logger.info("Updated the employees.");
 		} catch (Exception e1) {
+			logger.error("Error:", e1);
 			sendExceptionMail(e1);
-			e1.printStackTrace();
 		}
 
 	}
@@ -97,16 +112,18 @@ public class EmployeeDataFeeder {
 		try {
 			if("true".equalsIgnoreCase(mailEnableInCaseError)){
 				String exception = ExceptionUtils.getStackTrace(e1);
+				logger.info("Sending exception mail");
 				mailService.sendMail(mailList.split(","), null, null, "Induction Employee Data Process Exception", exception, null);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("Error:", e);
 		}
 	}
 
 	private List<Employee> getEmployeeFromExcel() throws Exception{
 		List<Employee> employees = null;
 
+		logger.info("Reading employees from "+employeeExcelPath);
 		try(	InputStream fs = new FileInputStream (new File(employeeExcelPath));
 				Workbook workbook = WorkbookFactory.create(fs) 
 				){
@@ -132,12 +149,13 @@ public class EmployeeDataFeeder {
 						employees.add(employee);
 					}
 				}catch(Exception e){
+					logger.error("Skipping the employee who do not have mandatory info in Excel", e);
 					//Skipping the employee who do not have mandatory info in Excel
 					//We can send exception mail here as well
 				}
 			}
 		}catch(Exception e){
-			e.printStackTrace();
+			logger.error("Error:", e);
 			throw e;
 		}
 		return employees;
@@ -160,8 +178,7 @@ public class EmployeeDataFeeder {
 			employee.setGender(row.getCell(12, Row.CREATE_NULL_AS_BLANK).getStringCellValue().trim());
 			employee.setAuthorities(row.getCell(13, Row.CREATE_NULL_AS_BLANK).getStringCellValue().trim());
 		} catch (Exception e) {
-			logger.error(e);
-			e.printStackTrace();
+			logger.error("Error:", e);
 			throw e;
 		}
 		return employee;
